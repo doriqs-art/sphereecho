fac3e2922219c7ef4e22136392dc2c1d1ae16b6a
doriqs-art <doriqs@gmail.com>
2026-06-06 13:16:24 +0300

    Update page.tsx

--- helmet-main/src/app/page.tsx
+++ helmet-main/src/app/page.tsx
@@ -1,4 +1,9 @@
-import { CodropScene } from "../components/CodropScene";
+import dynamic from "next/dynamic";
+
+const CodropScene = dynamic(
+  () => import("../components/CodropScene").then((mod) => mod.CodropScene),
+  { ssr: false }
+);
 
 export default function Home() {
   return <CodropScene />;
